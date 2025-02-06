a898e1ab9565e9ab33c0f3de397b6748ad0fb324
VitorPaes-coder <vpaes3197@gmail.com>
2025-02-06 10:48:06 -0300

    javascript ok

---
+++ app.js
@@ -0,0 +1,29 @@
+'use stript'
+
+const disciplinas = [
+    {nome: 'PPDM', img: 'android.png', cor: 'green'},
+    {nome: 'PRO', img: 'google.png', cor: 'blue'},
+    {nome: 'PWBE', img: 'netlfixx.png', cor: 'red'},
+    {nome: 'PWFE', img: 'shoppe.png', cor: 'orange'}
+   
+]
+
+function criarMenu(disciplina){
+    const novoItem = document.createElement('li')
+    const novoImagem = document.createElement('img')
+    const novoSpan = document.createElement('span')
+    const lista = document.getElementById('menu')
+
+    novoImagem.src = `./img/${disciplina.img}`
+    novoSpan.textContent = disciplina.nome
+
+    novoItem.appendChild(novoImagem)
+    novoItem.appendChild(novoSpan)
+    novoItem.style = `--cor-hover: ${disciplina.cor}`
+
+
+    lista.appendChild(novoItem)
+
+}
+
+disciplinas.forEach(criarMenu)
