8ed9af290eced5ea2f790d120697a9177510d6f1
VitorPaes-coder <vpaes3197@gmail.com>
2025-02-06 12:25:49 -0300

    uodo ok

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 'use stript'
+import produtos from "./listaProdutos"  
 
 const disciplinas = [
     {nome: 'PPDM', img: 'android.png', cor: 'green'},
@@ -10,6 +11,26 @@ const disciplinas = [
 
 function criarMenu(disciplina){
     const novoItem = document.createElement('li')
+    const novoImagem = document.createElement('img')
+    const novoSpan = document.createElement('span')
+    const lista = document.getElementById('menu')
+
+    novoImagem.src = `./img/${disciplina.img}`
+    novoSpan.textContent = disciplina.nome
+
+    novoItem.appendChild(novoImagem)
+    novoItem.appendChild(novoSpan)
+    novoItem.style = `--cor-hover: ${disciplina.cor}`
+
+
+    lista.appendChild(novoItem)
+
+}
+
+disciplinas.forEach(criarMenu)
+
+function criarCards(tee){
+    const novoItem = document.createElement('div')
     const novoImagem = document.createElement('img')
     const novoSpan = document.createElement('span')
     const lista = document.getElementById('menu')
---
+++ listaProdutos.js
@@ -0,0 +1,25 @@
+export const produtos = [
+    {
+        img: './camisetas/bomba.webp', 
+        nome: 'AtomiC Tee', 
+        descricao: 'Confeccionada em Suedine Preto 100% algodão 240GSM (alta gramatura) na modelagem CIENA, nossa t-shirt conta com estampa frontal e traseira em Silk.', 
+        cor: '#f5412f', 
+        preco: 219.0,
+        tamanhos: [P, M, G]},
+    {
+        img: './camisetas/muie.webp', 
+        nome: 'Spectacle Heavy Tee', 
+        descricao: 'Confeccionada em Suedine branco 100% algodão 220GSM (alta gramatura) na modelagem CIENA, nossa t-shirt conta com estampa frontal em silk.', 
+        cor: '#0039b2', 
+        preco: 219.0,
+        tamanhos: [P, M, G]},
+    {
+        img: './camisetas/simboloFaccao.webp', 
+        nome: 'Magic Trick Heavy Tee', 
+        descricao: 'Confeccionada em Suedine vermelho 100% algodão 220GSM (alta gramatura) na modelagem CIENA, nossa t-shirt conta com estampa frontal e traseira em silk.', 
+        cor: '#e7b41a', 
+        preco: 219.0,
+        tamanhos: [P, M, G]}
+]
+
+
